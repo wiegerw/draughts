08849e8ef7de91d48bfd9f0ccd1294f32c05e05a
Wieger Wesselink <j.w.wesselink@tue.nl>
2022-09-20 11:38:01 +0200

    Added a test for scan_search and endgame positions

---
+++ tests/test_search.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+#  (C) Copyright Wieger Wesselink 2022. Distributed under the GPL-3.0
+#  Software License, (See accompanying file license.txt or copy at
+#  https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import unittest
+from draughts1 import *
+
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        Scan.set("variant", "normal")
+        Scan.set("book", "false")
+        Scan.set("book-ply", "4")
+        Scan.set("book-margin", "4")
+        Scan.set("ponder", "false")
+        Scan.set("threads", "1")
+        Scan.set("tt-size", "24")
+        Scan.set("bb-size", "4")
+        Scan.update()
+        Scan.init()
+
+    def test_search(self):
+        text = '''
+           .   .   .   .   . 
+         .   .   .   x   .   
+           .   .   .   .   . 
+         .   .   .   .   .   
+           .   .   .   .   . 
+         .   .   .   .   .   
+           .   .   x   .   . 
+         .   .   .   .   .   
+           .   .   .   .   . 
+         O   .   .   O   O   W
+        '''
+        pos = parse_position(text)
+        display_position(pos)
+        print('eval', eval_position(pos))
+        max_depth = 15
+        max_time = 5.0
+        score, move = scan_search(pos, max_depth, max_time)
+        print(f'score = {score}, move = {print_move(move, pos)}')
+
+        text = '''
+           .   .   .   .   . 
+         .   .   .   o   .   
+           .   .   .   .   . 
+         .   .   .   .   .   
+           .   .   .   .   . 
+         .   .   .   .   .   
+           .   .   o   .   . 
+         .   .   .   .   .   
+           .   .   .   .   . 
+         X   .   .   X   X   B
+        '''
+        pos = parse_position(text)
+        display_position(pos)
+        print('eval', eval_position(pos))
+        max_depth = 15
+        max_time = 5.0
+        score, move = scan_search(pos, max_depth, max_time)
+        print(f'score = {score}, move = {print_move(move, pos)}')
+
+
+        text = '''
+           .   .   .   .   .
+         .   .   .   o   .
+           .   .   .   .   .
+         .   .   .   .   .
+           .   .   .   .   .
+         .   .   .   .   .
+           .   .   o   .   .
+         .   .   .   .   .
+           .   .   .   .   .
+         X   .   .   .   X   B
+        '''
+        pos = parse_position(text)
+        display_position(pos)
+        print('eval', eval_position(pos))
+        max_depth = 15
+        max_time = 5.0
+        score, move = scan_search(pos, max_depth, max_time)
+        print(f'score = {score}, move = {print_move(move, pos)}')
+
+
+        text = '''
+           .   .   .   O   .
+         .   .   .   .   .
+           .   .   .   .   .
+         .   .   .   .   .
+           .   .   .   .   .
+         .   .   .   .   .
+           .   .   o   .   .
+         .   .   .   .   .
+           .   .   .   .   .
+         X   .   .   .   X   B
+        '''
+        pos = parse_position(text)
+        display_position(pos)
+        print('eval', eval_position(pos))
+        max_depth = 15
+        max_time = 5.0
+        score, move = scan_search(pos, max_depth, max_time)
+        print(f'score = {score}, move = {print_move(move, pos)}')
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
+
